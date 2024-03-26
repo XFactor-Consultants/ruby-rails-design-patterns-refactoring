87414a0b0a7a8d53fd08df03e4f1faf4636d4aef
Henchworm <agop5134@gmail.com>
2024-03-26 08:43:11 -0600

    fixes api v1 helper typo

--- app/helpers/api/v1/wiki_posts_helper.rb
+++ app/helpers/api/v1/wiki_posts_helper.rb
@@ -6,7 +6,7 @@ module Api
       def parse_and_validate_json_request
         JSON.parse(request.body.read)
         request.body.rewind
-        recuse JSON::ParserError => e
+      rescue JSON::ParserError => e
         render json: { error: "Malformed JSON: #{e.message}" }, status: :bad_request
       end
     end
